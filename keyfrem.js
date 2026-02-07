e6327184962f0dfcf938b3e445d3993a45d384cd
abangidoy <tipalsyah123@gmail.com>
2026-02-07 10:45:54 +0700

    ini tampilan aja

--- keyfrem.js
+++ keyfrem.js
@@ -1,12 +1,1 @@
-document.addEventListener('DOMContentLoaded', () => {
 
-  const nav = document.querySelector('.navbar-sub ul');
-  if (!nav) return;
-
-  const spacer = document.createElement('li');
-  spacer.style.flex = '0 0 16px';
-  spacer.style.listStyle = 'none';
-
-  nav.appendChild(spacer);
-
-});
