9f8695e17b968c9dbf5395285ea866e38acd877a
tipalsyahhh <jinanauraa@gmail.com>
2026-02-10 17:58:07 +0700

    tampilan aja

---
+++ scrol.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded",()=>{
+
+const btn=document.getElementById("backToTop");
+
+btn.addEventListener("click",()=>{
+
+document.documentElement.scrollTop=0;
+document.body.scrollTop=0;
+
+});
+
+});
